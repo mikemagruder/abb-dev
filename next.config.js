66fe7e2c3b55b662c7d249d94c92031b2cef2c16
Mike Magruder <mike_magruder@yahoo.com>
2022-07-06 15:19:31 -0700

    redeployed abb

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  images: {
+    domains: ["links.papareact.com"],
+  },
+  env: {
+    mapbox_key: "pk.eyJ1IjoibWlrZW1hZ3J1ZGVyIiwiYSI6ImNsNTcxODMyazFseGEzZW1lZnhyYXMzZzMifQ.x98tzvSJmsqGbV9rx0jMNg",
+  },
+};
